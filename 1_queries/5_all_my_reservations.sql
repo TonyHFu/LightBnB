ab0e8d90973b6670a716725e27264202cea37f70
tofutigerz <hfu3@ualberta.ca>
2022-03-09 13:41:02 -0700

    finds all user reservations

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,19 @@
+SELECT reservations.id, title, start_date, cost_per_night, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN reservations ON (reservations.property_id = properties.id)
+JOIN property_reviews ON (property_reviews.property_id = properties.id)
+WHERE reservations.guest_id = 1
+GROUP BY  reservations.id, properties.id
+ORDER BY start_date
+LIMIT 10;
+
+--Answer from Compass
+
+-- SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+-- FROM reservations
+-- JOIN properties ON reservations.property_id = properties.id
+-- JOIN property_reviews ON properties.id = property_reviews.property_id
+-- WHERE reservations.guest_id = 1
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10;

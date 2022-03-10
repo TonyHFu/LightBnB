c006af2266434b529be3b5b2b5db1f90408ae05e
tofutigerz <hfu3@ualberta.ca>
2022-03-10 10:42:07 -0700

    refactors getAllProperties query to take options

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -123,11 +123,64 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool
-    .query(`
-      SELECT * FROM properties
-      LIMIT $1;
-    `, [limit])
+
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    WHERE true
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `
+      AND UPPER(city) LIKE UPPER($${queryParams.length})
+    `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `
+      AND properties.owner_id = $${queryParams.length}
+    `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `
+      AND properties.cost_per_night >= $${queryParams.length}
+    `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `
+      AND properties.cost_per_night <= $${queryParams.length}
+    `;
+  }
+
+  
+  queryString += `
+    GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${queryParams.length}
+    `;
+  }
+  
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+  
+  return pool.query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
